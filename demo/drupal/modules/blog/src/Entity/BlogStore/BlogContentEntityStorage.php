55710a2b136c04ac8c7e42d43ed65840e9a52d7e
Nick Schuch <nick@myschuch.com>
2016-10-25 14:16:11 +1000

    Fixes for update functionality

--- demo/drupal/modules/blog/src/Entity/BlogStore/BlogContentEntityStorage.php
+++ demo/drupal/modules/blog/src/Entity/BlogStore/BlogContentEntityStorage.php
@@ -29,13 +29,6 @@ class BlogContentEntityStorage extends StorageBase {
   protected $client;
 
   /**
-   * A quick and dirty list of blogs (in memory).
-   *
-   * @var array
-   */
-  protected $blogs;
-
-  /**
    * Overrides Drupal\Core\KeyValueStore\StorageBase::__construct().
    *
    * @param string $collection
@@ -64,28 +57,12 @@ class BlogContentEntityStorage extends StorageBase {
    */
   public function getMultiple(array $keys) {
     $result = $this->getAll();
-
     $values = array();
-
     foreach ($keys as $key) {
       if (isset($result[$key])) {
         $values[$key] = $result[$key];
       }
     }
-
-    /*try {
-      $result = $this->connection->query('SELECT name, value FROM {' . $this->connection->escapeTable($this->table) . '} WHERE name IN ( :keys[] ) AND collection = :collection', array(':keys[]' => $keys, ':collection' => $this->collection))->fetchAllAssoc('name');
-      foreach ($keys as $key) {
-        if (isset($result[$key])) {
-          $values[$key] = $this->serializer->decode($result[$key]->value);
-        }
-      }
-    }
-    catch (\Exception $e) {
-      // @todo: Perhaps if the database is never going to be available,
-      // key/value requests should return FALSE in order to allow exception
-      // handling to occur but for now, keep it an array, always.
-    }*/
     return $values;
   }
 
@@ -95,7 +72,7 @@ class BlogContentEntityStorage extends StorageBase {
   public function getAll() {
     $blogs = array();
 
-    $request = new Request('GET', 'http://172.18.0.2:8080/blogs');
+    $request = new Request('GET', 'http://api:8080/blogs');
 
     try {
       $response = $this->client->send($request);
@@ -108,9 +85,7 @@ class BlogContentEntityStorage extends StorageBase {
       }
     }
     catch (RequestException $e) {
-      // @todo: Perhaps if the database is never going to be available,
-      // key/value requests should return FALSE in order to allow exception
-      // handling to occur but for now, keep it an array, always.
+      throw $e;
     }
 
     return $blogs;
@@ -119,91 +94,56 @@ class BlogContentEntityStorage extends StorageBase {
   /**
    * {@inheritdoc}
    */
-  public function set($key, $value) {
-    /**
-     * array (
-     * 'id' =>
-     * array (
-     * 0 =>
-     * array (
-     * 'value' => '7JsMU5pwDrokKCtRW8kc',
-     * ),
-     * ),
-     * 'uuid' =>
-     * array (
-     * ),
-     * 'title' =>
-     * array (
-     * 0 =>
-     * array (
-     * 'value' => 'Test Blog 2',
-     * ),
-     * ),
-     * 'body' =>
-     * array (
-     * 0 =>
-     * array (
-     * 'value' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
-     * ),
-     * ),
-     * )
-     */
-    /*$this->connection->merge($this->table)
-      ->keys(array(
-        'name' => $key,
-        'collection' => $this->collection,
-      ))
-      ->fields(array('value' => $this->serializer->encode($value)))
-      ->execute();*/
+  public function set($key, $values) {
+    $request = new Request('POST', 'http://api:8080/blog/' . $key, [], json_encode(array(
+      'title' => $values['title'][0]['value'],
+      'body' => $values['body'][0]['value'],
+    )));
+    $request = $request->withHeader('Content-Type', 'application/json');
+
+    try {
+      $this->client->send($request);
+    }
+    catch (RequestException $e) {
+      throw $e;
+    }
   }
 
   /**
    * {@inheritdoc}
    */
-  public function setIfNotExists($key, $value) {
-    /*$result = $this->connection->merge($this->table)
-      ->insertFields(array(
-        'collection' => $this->collection,
-        'name' => $key,
-        'value' => $this->serializer->encode($value),
-      ))
-      ->condition('collection', $this->collection)
-      ->condition('name', $key)
-      ->execute();
-    return $result == Merge::STATUS_INSERT;*/
+  public function setIfNotExists($key, $values) {
+    $this->set($key, $values);
   }
 
   /**
    * {@inheritdoc}
    */
   public function rename($key, $new_key) {
-    /*$this->connection->update($this->table)
-      ->fields(array('name' => $new_key))
-      ->condition('collection', $this->collection)
-      ->condition('name', $key)
-      ->execute();*/
+    // Not supported.
   }
 
   /**
    * {@inheritdoc}
    */
   public function deleteMultiple(array $keys) {
-    // Delete in chunks when a large array is passed.
-    /*while ($keys) {
-      $this->connection->delete($this->table)
-        ->condition('name', array_splice($keys, 0, 1000), 'IN')
-        ->condition('collection', $this->collection)
-        ->execute();
-    }*/
+    foreach ($keys as $key) {
+      $request = new Request('DELETE', 'http://api:8080/blog/' . $key);
+      try {
+        $this->client->send($request);
+      }
+      catch (RequestException $e) {
+        throw $e;
+      }
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function deleteAll() {
-    /*$this->connection->delete($this->table)
-      ->condition('collection', $this->collection)
-      ->execute();*/
+    $blogs = $this->getAll();
+    $this->deleteAll(array_keys($blogs));
   }
 
 }
